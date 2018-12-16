52cd22f7449140893c9f65c10ebd4ea18a44056b
Sebastian Knackstedt <sebknack@gmail.com>
2018-12-16 18:28:18 +0100

    Added basic functionality.

--- app/src/main/java/nuclear001_productions/whatsmydata/ReceiverActivity.java
+++ app/src/main/java/nuclear001_productions/whatsmydata/ReceiverActivity.java
@@ -1,21 +1,68 @@
 package nuclear001_productions.whatsmydata;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReceiverActivity extends AppCompatActivity {
+
+    private TextView textViewMessageCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receiver);
 
-        Intent incomingData = getIntent(); // <<--- Breakpoint
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(incomingData.toString());
-        builder.show();
+        //Init layout
+        textViewMessageCounter = findViewById(R.id.text_view_message_counter);
+
+
+        Intent incomingData = getIntent();
+        final Uri uri1 = incomingData.getClipData().getItemAt(0).getUri();
+
+        ReceiverViewModel viewModel = ViewModelProviders.of(this, new ViewModelProvider.Factory() {
+            @NonNull
+            @Override
+            @SuppressWarnings("unchecked")
+            public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+                try {
+                    return (T) new ReceiverViewModel(getContentResolver().openInputStream(uri1));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return (T) new ReceiverViewModel(null);
+            }
+        }).get(ReceiverViewModel.class);
+        viewModel.getData().observe(this, new Observer<List<String>>() {
+            @Override
+            public void onChanged(@Nullable List<String> strings) {
+                analyzeData(strings);
+            }
+        });
     }
+
+    public void analyzeData(List<String> chat) {
+        // Do stuff
+        System.out.println("It worked!");
+        textViewMessageCounter.setText("Nachrichten: " + chat.size());
+    }
+
 }
---
+++ app/src/main/java/nuclear001_productions/whatsmydata/ReceiverViewModel.java
@@ -0,0 +1,52 @@
+package nuclear001_productions.whatsmydata;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReceiverViewModel extends ViewModel {
+
+    private MutableLiveData<List<String>> data;
+    private InputStream chatStream;
+
+    public ReceiverViewModel(InputStream stream) {
+        this.data = new MutableLiveData<>();
+        this.chatStream = stream;
+        new ChatParser().execute();
+    }
+
+    public LiveData<List<String>> getData() {
+        return data;
+    }
+
+    private class ChatParser extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            ArrayList<String> chatList = new ArrayList<>();
+            try {
+                BufferedReader stream = new BufferedReader(new InputStreamReader(chatStream));
+
+                String line;
+
+                while ((line = stream.readLine()) != null) {
+                    chatList.add(line);
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            data.postValue(chatList);
+            return null;
+        }
+    }
+}
